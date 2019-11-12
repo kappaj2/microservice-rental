e00f391a25ac8c7f593d22278b4352973fa94042
Andre <kappaj@gmail.com>
2019-11-12 19:40:04 +0200

    Adding read of properties from config server

---
+++ jdbc-auth-server/src/main/java/za/co/ajk/rentcloud/authorizationserver/config/JdbcAuthServerProfileConfiguration.java
@@ -0,0 +1,22 @@
+package za.co.ajk.rentcloud.authorizationserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JdbcAuthServerProfileConfiguration implements CommandLineRunner {
+
+    @Autowired
+    private Environment environment;
+
+    public String getUrl(){
+        return environment.getProperty("spring.datasource.url");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("URL Value : "+getUrl());
+    }
+}
